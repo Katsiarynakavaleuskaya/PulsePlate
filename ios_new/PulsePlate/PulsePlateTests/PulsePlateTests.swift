28623291512c3aaa8094a05a2e702427f34b871f
Katsiarynakavaleuskaya <lexakm532@gmail.com>
2025-09-28 13:21:32 +0300

    feat(ios): create new Xcode project with all Swift files
    - Created new Xcode project in ios_new/PulsePlate/
    - Added all Swift files: Views, Models, Components, Tests
    - Added Assets.xcassets, Info.plist, LaunchScreen.storyboard
    - Added localization files (en.lproj, ru.lproj)
    - Created backup of old ios files in ios_backup/
    - Project structure ready for compilation and testing

---
+++ ios_new/PulsePlate/PulsePlateTests/PulsePlateTests.swift
@@ -0,0 +1,16 @@
+//
+//  PulsePlateTests.swift
+//  PulsePlateTests
+//
+//  Created by katsiaryna kavaleuskaya on 28.09.25.
+//
+
+import Testing
+
+struct PulsePlateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
