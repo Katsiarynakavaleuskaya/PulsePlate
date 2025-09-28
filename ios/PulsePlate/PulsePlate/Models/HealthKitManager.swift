f77e02323c6517b68ca96eb7d530b7ee28c94605
Katsiarynakavaleuskaya <lexakm532@gmail.com>
2025-09-28 18:19:34 +0300

    Update HealthKitManager, APITests, and database_versions.json

--- ios/PulsePlate/PulsePlate/Models/HealthKitManager.swift
+++ ios/PulsePlate/PulsePlate/Models/HealthKitManager.swift
@@ -1,9 +1,11 @@
 import Foundation
 import HealthKit
+import Combine
 
 enum HealthKitError: Error, LocalizedError {
     case notAvailable
     case authorizationDenied
+    case invalidObjectType
 
     var errorDescription: String? {
         switch self {
@@ -11,6 +13,8 @@ enum HealthKitError: Error, LocalizedError {
             return "HealthKit is not available on this device"
         case .authorizationDenied:
             return "HealthKit authorization was denied"
+        case .invalidObjectType:
+            return "One or more HealthKit object types are invalid or unavailable"
         }
     }
 }
@@ -26,15 +30,20 @@ class HealthKitManager: ObservableObject {
             return
         }
 
-        let typesToRead: Set<HKObjectType> = [
-            HKObjectType.quantityType(forIdentifier: .dietaryEnergyConsumed)!,
-            HKObjectType.quantityType(forIdentifier: .dietaryProtein)!,
-            HKObjectType.quantityType(forIdentifier: .dietaryCarbohydrates)!,
-            HKObjectType.quantityType(forIdentifier: .dietaryFatTotal)!,
-            HKObjectType.quantityType(forIdentifier: .dietaryFiber)!,
-            HKObjectType.quantityType(forIdentifier: .dietarySugar)!,
-            HKObjectType.quantityType(forIdentifier: .dietarySodium)!
+        let identifiers: [HKQuantityTypeIdentifier] = [
+            .dietaryEnergyConsumed,
+            .dietaryProtein,
+            .dietaryCarbohydrates,
+            .dietaryFatTotal,
+            .dietaryFiber,
+            .dietarySugar,
+            .dietarySodium
         ]
+        let typesToRead: Set<HKObjectType> = Set(identifiers.compactMap { HKObjectType.quantityType(forIdentifier: $0) })
+        if typesToRead.count != identifiers.count {
+            error = HealthKitError.invalidObjectType
+            return
+        }
 
         healthStore.requestAuthorization(toShare: nil, read: typesToRead) { [weak self] success, error in
             DispatchQueue.main.async {
@@ -50,10 +59,12 @@ class HealthKitManager: ObservableObject {
         let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
 
         let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: endOfDay, options: .strictStartDate)
-
+        guard let sampleType = HKObjectType.quantityType(forIdentifier: .dietaryEnergyConsumed) else {
+            throw HealthKitError.invalidObjectType
+        }
         return try await withCheckedThrowingContinuation { continuation in
             let query = HKSampleQuery(
-                sampleType: HKObjectType.quantityType(forIdentifier: .dietaryEnergyConsumed)!,
+                sampleType: sampleType,
                 predicate: predicate,
                 limit: HKObjectQueryNoLimit,
                 sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: true)]
