c9ee976de3d003f58a88a0f7bb35beddcd3a0640
Katsiarynakavaleuskaya <lexakm532@gmail.com>
2025-09-29 17:56:05 +0300

    fix: Исправления Swift приложения - AnimationTests и обновление иконок
    - Исправлен AnimationTests.swift для корректной работы тестов
    - Обновлен Package.swift для iOS зависимостей
    - Добавлены новые иконки приложения AppIcon_Luxury_PulsePlate.zip
    - Удалены старые файлы анимаций и иконок

--- ios/Package.swift
+++ ios/Package.swift
@@ -3,8 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "PulsePlate",
+    defaultLocalization: "en",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v17),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
@@ -20,6 +22,11 @@ let package = Package(
             name: "PulsePlate",
             dependencies: [
                 .product(name: "Lottie", package: "lottie-ios")
+            ],
+            path: "PulsePlate",
+            resources: [
+                .process("Assets.xcassets"),
+                .process("Resources")
             ]
         ),
     ]
